95f4600b35dbcb91b6f7a2b0d1264f5650bdc2a2
isnullxbh <isnullxbh@gmail.com>
2019-01-18 01:40:41 +0700

    [chore] socket base implementation

---
+++ src/xne/sockets/socket_base.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file    socket_base.cpp
+ * @author  isnullxbh
+ * @date    2019-01-16 11:27
+ * @version 0.0.1
+ */
+
+#include "xne/sockets/socket_base.h"
+
+namespace xne {
+namespace net {
+
+const socket_base::handle_type socket_base::invalid_handle = -1;
+
+} // namespace net
+} // namespace xne
