b5ee3362e7faf90d8754407caaea44cf38a87a3e
isnullxbh <isnullxbh@gmail.com>
2019-02-14 12:02:48 +0700

    [chore] remove protocol-specific methods

--- include/xne/sockets/basic_acceptor.h
+++ include/xne/sockets/basic_acceptor.h
@@ -19,10 +19,12 @@ template<typename Protocol>
 class basic_acceptor {
 public:
     using protocol_type = Protocol;
-    using socket_type = typename Protocol::socket_type;
+    using socket_type = typename protocol_type::socket_type;
+    using endpoint_type = typename protocol_type::endpoint_type;
+    using handle_type = socket_base::handle_type;
 
 public:
-    basic_acceptor(const protocol_type& proto, socket_type* socket);
+    basic_acceptor(const protocol_type& proto, socket_type&& socket);
     virtual ~basic_acceptor() = default;
 
     basic_acceptor(const basic_acceptor&) = delete;
@@ -31,35 +33,20 @@ public:
     basic_acceptor& operator=(basic_acceptor&&) = delete;
 
 public:
-    bool bind(std::error_code& ec);
-    socket_base::handle_type accept(std::error_code& ec);
-    bool listen(size_t maxconns, std::error_code& ec);
+    virtual bool bind(const endpoint_type& endpoint, std::error_code& ec) = 0;
+    virtual handle_type accept(endpoint_type& remote_endpoint, std::error_code& ec) = 0;
+    virtual bool listen(int backlog, std::error_code& ec) = 0;
 
 protected:
     protocol_type   proto_;
-    socket_type*    socket_;
+    socket_type     socket_;
 };
 
 template<typename Protocol>
-inline basic_acceptor<Protocol>::basic_acceptor(const protocol_type& proto, socket_type* socket)
+inline basic_acceptor<Protocol>::basic_acceptor(const protocol_type& proto, socket_type&& socket)
     : proto_(proto)
-    , socket_(socket)
+    , socket_(std::move(socket))
 {}
-
-template<typename Protocol>
-inline bool basic_acceptor<Protocol>::bind(std::error_code& ec) {
-    return syscall::bind(socket_->native_handle(), ec);
-}
-
-template<typename Protocol>
-inline bool basic_acceptor<Protocol>::listen(size_t maxconns, std::error_code& ec) {
-    return syscall::listen(socket_->native_handle(), maxconns, ec);
-}
-
-template<typename Protocol>
-inline socket_base::handle_type basic_acceptor<Protocol>::accept(std::error_code& ec) {
-    return syscall::accept(socket_->native_handle(), ec);
-}
 
 } // namespace net
 } // namespace xne
